9c86021bc05dfa461e763c75ae532e7837f94f58
Rocksampath12 <zakkulasampath@gmail.com>
2025-03-26 11:48:33 +0000

    Pending image to display while fetch rejected

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -8,29 +8,32 @@ const VaccinationByAge = props => {
   const colors = ['#fecba6', '#b3d23f', '#a44c9e', '#ff6347', '#4682b4'] // Add more colors if needed
 
   return (
-    <ResponsiveContainer width="100%" height={300}>
-      <PieChart>
-        <Pie
-          cx="50%"
-          cy="50%"
-          data={VaccinationByAgeData}
-          startAngle={0}
-          endAngle={360}
-          innerRadius="40%"
-          outerRadius="70%"
-          dataKey="count"
-          nameKey="age"
-        >
-          {VaccinationByAgeData.map(entry => (
-            <Cell
-              key={entry.age}
-              fill={colors[parseInt(entry.age) % colors.length]}
-            />
-          ))}
-        </Pie>
-        <Legend iconType="circle" layout="horizontal" verticalAlign="bottom" />
-      </PieChart>
-    </ResponsiveContainer>
+    <>
+      <ResponsiveContainer width="100%" height={300}>
+        <PieChart>
+          <Pie
+            cx="50%"
+            cy="50%"
+            data={VaccinationByAgeData}
+            startAngle={0}
+            endAngle={360}
+            innerRadius="40%"
+            outerRadius="70%"
+            dataKey="count"
+            nameKey="age"
+          >
+            {VaccinationByAgeData.map((entry, index) => (
+              <Cell key={entry.age} fill={colors[index % colors.length]} />
+            ))}
+          </Pie>
+          <Legend
+            iconType="circle"
+            layout="horizontal"
+            verticalAlign="bottom"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </>
   )
 }
 
